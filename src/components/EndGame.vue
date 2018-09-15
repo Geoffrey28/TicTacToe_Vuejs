afd7bf83f190af3805a58da8a4e98fecb9a26a5e
Geoffrey28 <geoffreygirard28@gmail.com>
2018-09-15 15:20:17 +0200

    add Reset game

--- src/components/EndGame.vue
+++ src/components/EndGame.vue
@@ -1,12 +1,39 @@
 <template>
-    <div>
-        EndGame
+    <div class="endgame">
+        <p>Winner is</p>
+        <p>{{ this.winner }}</p>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'EndGame'
+    name: 'EndGame',
+    data () {
+        return {
+            winner: 'null'
+        }
+    }
 }
 </script>
+
+<style lang="scss" scoped>
+.endgame {
+    width: 500px;
+    height: 500px;
+    text-align: center;
+    color: rgb(80, 220, 140);
+    font-size: 3em;
+    animation: text_anim 1.5s cubic-bezier(.61,1.17,.82,.95);
+    & p:first-of-type {
+        margin-top: 20vh;
+    }
+}
+
+@keyframes text_anim {
+    0% {
+        font-size: 0;
+    }
+}
+</style>
+
 
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -9,6 +9,9 @@
           <p>Player 2</p>
           <p class="score">{{ this.score.two }}</p>
         </div>
+        <div class="reset">
+          <router-link to="/game">Reset</router-link>
+        </div>
       </div>
       <router-view v-on:win="getScore()"></router-view>
   </div>
@@ -26,18 +29,20 @@ export default {
       score: {
         one: 0,
         two: 0
-      }
+      },
+      win: false
     }
   },
   methods: {
     getScore () {
-      if (null == 'cross') {
+      if ('cross' == 'cross') {
         this.score.one++
       }
       if (null == 'circle') {
         this.score.two++
       }
       this.$router.push('/end')
+      this.win = true
     }
   }
 }
@@ -62,6 +67,26 @@ export default {
       .score {
         text-align: center;
       }
+    }
+  }
+  .reset {
+    width: 150px;
+    height: 40px;
+    border: 1px solid rgb(80, 220, 140);
+    font-size: 1.4em;
+    border-radius: 20%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &:hover {
+      background: rgb(80, 220, 140);
+      & a {
+        color: #000;
+      }
+    }
+    a {
+    color: rgb(80, 220, 140);
+    text-decoration: none;
     }
   }
 }
